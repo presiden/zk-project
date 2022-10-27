ba9640a970b8e9c00d94d0b93d43a96f4b0477c2
Prasidyo <prasidyo.adi@nostratech.com>
2022-10-27 21:39:21 +0700

    initial commit

---
+++ src/main/java/com/example/zkproject/ZkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.zkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zkproject/ZkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
